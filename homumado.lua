2d693ace2aad922a7ec457454fdc2ab21c39f460
iLove-yuri <risaxmiyayuri@gmail.com>
2026-05-02 17:49:35 +0700

    Add files via upload

--- homumado.lua
+++ homumado.lua
@@ -11,7 +11,8 @@ local list = {
 	["5130394318"] = {id = "1b1251046fd4407c1d8f7e90cb337aeb", keyless = false }, --- bizarre lineage
 	["7359962123"] = {id = "606ff7b9ea1d1c897761b2f0c218f3d9", keyless = false }, --- aac
 	["1709917610"] = {id = "1a8a31e1f73770905420267556ed741d", keyless = false }, -- regretevator
-	["9186719164"] = {id = "2e85411f515c33094f504d7c6b198a35", keyless = true } --- sailor piece
+	["9186719164"] = {id = "2e85411f515c33094f504d7c6b198a35", keyless = true }, --- sailor piece
+	["9792947201"] = {id = "d8e39dd7c8bfa5015a2c48dc361d656f", keyless = false } --- slime rng
 }
 local executor_name = getexecutorname():match("^%s*(.-)%s*$")
 local game_id = tostring(game.GameId)
