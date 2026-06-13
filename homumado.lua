118c95a9107d172efb8ab2fd3cd8224b6760ecda
iLove-yuri <risaxmiyayuri@gmail.com>
2026-06-13 18:46:48 +0700

    Add files via upload

--- homumado.lua
+++ homumado.lua
@@ -35,7 +35,7 @@ local list = {
 	["10006104044"] = {id = "245e817ec11f0591898dbef698f5a598", keyless = false }, -- chinese
 	["10077613599"] = {id = "d310529bba1c9560607c620cc8664b89", keyless = false }, --- tef
 	["9584946743"] = {id = "d8e39dd7c8bfa5015a2c48dc361d656f", keyless = false }, --- Immortality
-	["10111742174"] = {id = "74b4e982b9b980d106fc43e8ca53f248", keyless = true }, --- 🧱
+	["10111742174"] = {id = "74b4e982b9b980d106fc43e8ca53f248", keyless = false }, --- 🧱
 	["7395930870"] = {id = "3781eb1fc444bef291a013c0e69f7c2a", keyless = false}, --lemon
 	["9965411707"] = {id = "79c4f538aba5d702cd1b7795737a36d1", keyless = false }, --ni
 }
