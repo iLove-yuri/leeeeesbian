ea17b2af83200db5a14cb613e31fb33a31094a7f
iLove-yuri <risaxmiyayuri@gmail.com>
2026-09-12 15:36:09 +0700

    Add files via upload

--- homumado.lua
+++ homumado.lua
@@ -27,7 +27,7 @@ local UserInputService = Services.UserInputService
 local RunService = Services.RunService
 local Plr = Players.LocalPlayer
 local Lst = {
-	{gid = {"1831550657", "10337069275", "10399524075", "3512256796", "10323871857", "10454554751", "6324644175", "10531477636", "10219827882", "10647180866"}, id = "1b1251046fd4407c1d8f7e90cb337aeb", keyless = false}, -- cos, ice tycoon, golem, minor, zenith, unbox, mp, auto, piggy
+	{gid = {"1831550657", "10337069275", "10399524075", "3512256796", "10764369335", "10454554751", "6324644175", "10531477636", "10219827882", "10647180866"}, id = "1b1251046fd4407c1d8f7e90cb337aeb", keyless = false}, -- cos, ice tycoon, golem, minor, zenith, unbox, mp, auto, piggy
 	{gid = {"7359962123"}, id = "f3cdf28dc70b1249611f4d9e92b15c4e", keyless = false}, -- aac
 	{gid = {"10759415852", "8978470369", "9870850309", "10529769390", "7613921865","3913007563", "10754208508", "1962399895", "10516888336", "10417987249", "9866884975", "10428418645"}, id = "245e817ec11f0591898dbef698f5a598", keyless = false}, -- gambling, castle, bubble, r, ae, tbb, balllslslsl, td, neb, syb, trench
 	{gid = {"9219838330", "10334731049", "6749892429", "7990186056", "7300616172", "10393620082", "9927195474", "10402792340", "7585140258", "7738524573", "7970033072"}, id = "d310529bba1c9560607c620cc8664b89", keyless = false}, -- pml, spm, eve, tower, purge, asmr, fish, dumpling, runaways, rtd
